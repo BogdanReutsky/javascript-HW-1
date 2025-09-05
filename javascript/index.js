ea62c4cfdcbfd2bd59fd43ca22f7763d3f934964
BogdanReutsky <reutbodya@gmail.com>
2025-09-05 14:38:00 +0300

    push

---
+++ javascript/index.js
@@ -0,0 +1,23 @@
+const age = "16";
+console.log(age);;
+const name = "Bogdan";
+console.log(name);;
+const isStudent = 1 > 0;
+console.log(isStudent);
+const myString = "EMPTY";
+console.log(myString);
+const myNumber = 12 + 10;
+console.log(myNumber);
+let myNull = null;
+console.log(myNull);
+const userName = prompt("Введіть своє ім'я");
+console.log(userName);
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer);
+alert("Увага! дія може бути небезпечною")
+confirm("Ви впевнені, що хочете продовжити?")
+
+
+
+
+
